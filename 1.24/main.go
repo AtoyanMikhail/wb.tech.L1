cc78e66c0ac5d76258cff0f6eebb0226d0d383cf
AtoyanMikhail <mikhail.atoyan@gmail.com>
2025-08-05 10:20:15 +0300

    fixed 1.24

--- 1.24/main.go
+++ 1.24/main.go
@@ -13,13 +13,13 @@ func NewPoint(x, y float64) *Point {
 	return &Point{x, y}
 }
 
-func Distance(a, b Point) float64 {
-	return math.Sqrt(math.Pow(math.Abs(a.x - b.x), 2) + math.Pow(math.Abs(a.y - b.y), 2))
+func (p Point) Distance(another Point) float64 {
+	return math.Sqrt(math.Pow(math.Abs(p.x - another.x), 2) + math.Pow(math.Abs(p.y - another.y), 2)) 
 }
 
 func main() {
 	a := NewPoint(0, 0)
 	b := NewPoint(1, 1)
-	dist := Distance(*a, *b)
-	fmt.Printf("Distance: %.4f", dist)
+	dist := a.Distance(*b)
+	fmt.Printf("Distance: %.4f\n", dist)
 }
